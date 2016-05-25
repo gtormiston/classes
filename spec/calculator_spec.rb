7734526965932a76661271c030b6268a91282fe4
Graham Ormiston <grahamormiston@gmail.com>
2016-05-25 11:36:59 +0100

    refactored into calculator into classes

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,33 +1,58 @@
 require 'calculator'
 
-describe '#add' do
+
+describe Calculator do
+  calc = Calculator.new
+  it 'creates a calculator object' do 
+    expect(calc).to be_a(Calculator)
+  end 
+
   it 'adds two numbers' do
-    expect(add(2, 3)).to eq 5
-    expect(add(6, 4)).to eq 10
-    expect(add(0, 0)).to eq 0
-    expect(add(-1, -1)).to eq -2
-    expect(add(Float::INFINITY, 0)).to eq Float::INFINITY
-    expect(add(Float::INFINITY, Float::INFINITY)).to eq Float::INFINITY
+    expect(calc.add(2, 3)).to eq 5
+   expect(calc.add(6, 4)).to eq 10
+   expect(calc.add(0, 0)).to eq 0
+   expect(calc.add(-1, -1)).to eq -2
+   expect(calc.add(Float::INFINITY, 0)).to eq Float::INFINITY
+   expect(calc.add(Float::INFINITY, Float::INFINITY)).to eq Float::INFINITY
+  end  
+
+   it 'subtracts two numbers' do
+    expect(calc.subtract(3, 2)).to eq 1
+    expect(calc.subtract(6, 4)).to eq 2
+    expect(calc.subtract(0, 0)).to eq 0
+    expect(calc.subtract(-1, -1)).to eq 0
+    expect(calc.subtract(Float::INFINITY, 0)).to eq Float::INFINITY
+    expect(calc.subtract(Float::INFINITY, Float::INFINITY)).to be_nan
   end
+
+  it 'divides two numbers' do
+    expect(calc.divide(10, 5)).to eq 2
+    expect(calc.divide(20, 2)).to eq 10
+  end
+
+  it 'multiplies two numbers' do
+    expect(calc.multiply(3, 2)).to eq 6
+    expect(calc.multiply(6, 4)).to eq 24
+  end
+
+
 end
 
-describe '#subtract' do
-  it 'subtracts two numbers' do
-    expect(subtract(3, 2)).to eq 1
-    expect(subtract(6, 4)).to eq 2
-    expect(subtract(0, 0)).to eq 0
-    expect(subtract(-1, -1)).to eq 0
-    expect(subtract(Float::INFINITY, 0)).to eq Float::INFINITY
-    expect(subtract(Float::INFINITY, Float::INFINITY)).to be_nan
-  end
-end
+describe Output do
+  output = Output.new
 
-describe '#print_answer' do
   it 'pretty prints the answer' do
-    expect(print_answer(4)).to eq "The Answer is: 4"
-    expect(print_answer(6)).to eq "The Answer is: 6"
-    expect(print_answer(-1)).to eq "The Answer is: -1"
-    expect(print_answer(0)).to eq "The Answer is: 0"
-    expect(print_answer(Float::INFINITY)).to eq "The Answer is: Infinity"
+    expect(output.print_answer(4)).to eq "The Answer is: 4"
+    expect(output.print_answer(6)).to eq "The Answer is: 6"
+    expect(output.print_answer(-1)).to eq "The Answer is: -1"
+    expect(output.print_answer(0)).to eq "The Answer is: 0"
+    expect(output.print_answer(Float::INFINITY)).to eq "The Answer is: Infinity"
   end
+
+  it 'takes calculator outputs and prints them' do
+    expect(output.print_answer(Calculator.new.add(2,2))).to eq "The Answer is: 4"
+    expect(output.print_answer(Calculator.new.subtract(10,4))).to eq "The Answer is: 6"
+  end
+
+
 end
